1eb417629ed29cadec48b2d3e439f9ffa47a1418
hamza <hamza.elmansouri.e@gmail.com>
2025-04-29 23:40:04 +0100

    edit Article and User

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -1,13 +1,12 @@
-<?php
-<<<<<<< HEAD
-namespace App\Models;
-use CodeIgniter\Model;
+<!-- 
+// namespace App\Models;
+// use CodeIgniter\Model;
 
-class UserModel extends Model{
-    protected $table = 'db_users';
-    protected $primaryKey = 'id';
-    protected $allowedFields = ['name','email'];
-}?>
+// class UserModel extends Model{
+//     protected $table = 'db_users';
+//     protected $primaryKey = 'id';
+//     protected $allowedFields = ['name','email'];
+// }?>
 =======
  namespace App\Models;
  use CodeIgniter\Model;
@@ -16,5 +15,4 @@ class UserModel extends Model{
      protected $table = 'users';
      protected $primaryKey = 'id';
      protected $allowedFields = ['name','email'];
- }?>
->>>>>>> 6cacc1df8cc14f9566ada4119cf1fb6f4763b6d6
+ }?> -->
